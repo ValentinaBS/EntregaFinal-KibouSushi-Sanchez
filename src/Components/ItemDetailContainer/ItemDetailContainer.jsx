27161525b229fd377d85804b7b80f2ebbadad1d8
ValentinaBS <vbsanchez16@gmail.com>
2023-05-09 14:47:48 -0300

    add error message if itemid is invalid

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa'
 import { getFirestore, doc, getDoc } from 'firebase/firestore'
 import ItemDetail from './ItemDetail'
 
@@ -17,12 +18,23 @@ export default function ItemDetailContainer() {
             setLoading(false)
         })
         },[itemId])
+    console.log(item)
 
     return (
         <>
-            {loading 
-            ? <h1 className='loading'>Loading...</h1>
-            : <ItemDetail item={item} />
+            {loading || !item.name
+            ?   <div className='detail__warning'>
+                <h1>
+                    We couldn't find the item you were looking for...
+                </h1>
+                <Link to='/'>
+                    <button className='error__button'>
+                        Go to home
+                        <FaHome className='error__button__icon'/>
+                    </button>
+                </Link>
+                </div>
+            :   <ItemDetail item={item} />
             }
         </>
     )
